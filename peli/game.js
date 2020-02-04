7082c97bb4d364c329a059b56260d6ebfefe5b0f
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2020-02-04 20:59:49 +0200

    Matopelin alkua

--- peli/game.js
+++ peli/game.js
@@ -1,11 +1,58 @@
-import { block } from './main.js'
+import { clear, block, keys } from './main.js'
 
-let x = 1
-
-export function frame(t) {
-	console.log('moi peli')
-
-	block(x, 0)
-
+function print(...args) {
+	console.log(...args)
 }
 
+let x = 0
+let y = 2
+let xvelocity = 1
+let yvelocity = 0
+
+export function frame() {
+	// Piirto
+	clear()
+	block(x, y)
+
+	// Näppäimet vasen/oikea
+	if (keys.left) {
+		xvelocity = -1
+		yvelocity = 0
+	}
+	if (keys.right) {
+		xvelocity = 1
+		yvelocity = 0
+	}
+	// Näppäimet ylös/alas
+	if (keys.up) {
+		yvelocity = -1
+		xvelocity = 0
+	}
+	if (keys.down) {
+		yvelocity = 1
+		xvelocity = 0
+	}
+
+	// Liikkuminen
+	x = x + xvelocity
+	y = y + yvelocity
+
+	// Menikö yläseinän läpi?
+	if (y < 0) {
+		y = 13
+	}
+	// Menikö alaseinän läpi?
+	if (y > 13) {
+		y = 0
+	}
+	// Menikö vasemman seinän läpi?
+	if (x < 0) {
+		x = 13
+	}
+	// Menikö oikean seinän läpi?
+	if (x > 13) {
+		x = 0
+	}
+}
+
+print('Jee tää funktio toimii!')
--- peli/main.js
+++ peli/main.js
@@ -1,15 +1,46 @@
-console.log('moi')
-
 let c = document.querySelector('canvas')
-
 let context = c.getContext('2d')
+
+export function clear() {
+    context.clearRect(0, 0, 1410, 1410)
+}
 
 export function block(x, y) {
     context.fillRect(10 + x * 100, 10 + y * 100, 90, 90)
 }
 
+export let keys = {
+    left: false,
+    right: false,
+    up: false,
+    down: false,
+}
+
 import { frame } from './game.js'
 
-frame()
+window.addEventListener('keydown', (ev) => {
+    if (ev.keyCode === 37) {
+        keys.left = true
+    }
+    if (ev.keyCode === 39) {
+        keys.right = true
+    }
+    if (ev.keyCode === 38) {
+        keys.up = true
+    }
+    if (ev.keyCode === 40) {
+        keys.down = true
+    }
+})
 
-    
+let SPEED = 6
+setInterval(function() {
+    frame()
+
+    for (let key in keys) {
+        keys[key] = false
+    }
+
+}, 1000 / 6)
+
+frame()
